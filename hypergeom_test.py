6ed95a6452b9e4d6c62020fac37a7601495a71fb
Ishan Gaur <ishangaur4@gmail.com>
2023-11-28 18:55:44 -0800

    hyper test for the sc maps directly

--- hypergeom_test.py
+++ hypergeom_test.py
@@ -1,3 +1,4 @@
+import pickle as pkl
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -13,41 +14,34 @@ import networkx as nx
 def jaccard(setA, setB):
     return len(setA.intersection(setB)) / len(setA.union(setB))
 
-def run_hypergeo_enrichr(ont_ts, hierarchygenes, ref_file, fdr_thre=0.01, ji_thre = 0.4, minCompSize=4):  
+# def run_hypergeo_enrichr(ont_ts, ref_df, gene_col="genes", fdr_thre=0.01, ji_thre = 0.4, minCompSize=4):  
+def run_hypergeo_enrichr(test_df, ref_df, gene_col="CD_MemberList", fdr_thre=0.01, ji_thre = 0.4, minCompSize=4):  
     '''
-    ont_ts: the result hierarchy from the community detection 
+    BOTH DATAFRAMES SHOULD BE SORTED BY SIZE IN DESCENDING ORDER
+    test_df: the result hierarchy from the community detection 
     hierarchy genes: total number of genes in the root of the hierarchy 
-    ref_file: the reference cellular component/protein complexes table 
+    ref_df: the reference cellular component/protein complexes table 
     fdr_thre: the fdr cutoff to be collected in the enriched terms (default = 0.01)
     ji_thre: the threshold for Jaccard Idex (default = 0, will set the threshold in the organization step)
     minCompSize: the smallest component size to be considered for the enrichment (default = 4)
     '''
     
-    M = len(hierarchygenes)
+    M = len(ref_df[gene_col].iloc[0].split(' '))
+    hyper_df = pd.DataFrame(index=test_df.index, columns=ref_df.index, dtype=float)
+    ji_df = pd.DataFrame(index=test_df.index, columns=ref_df.index, dtype=float)
 
-    track = 0
-    ref_df = pd.DataFrame(index=ont_ts.index, columns=ref_file.index, dtype=float)
-    ji_df = pd.DataFrame(index=ont_ts.index, columns=ref_file.index, dtype=float)
-    for ont_comp, ont_row in tqdm(ont_ts.iterrows(), total=ont_ts.shape[0]):
-        track += 1
-        ont_comp_genes = ont_row['genes'].split(' ')
-        n = ont_row['tsize']
-        for comp, row in ref_file.iterrows():
-            comp_genes = row['genes'].split(' ')
-            N = row['tsize']
+    for ont_comp, ont_row in tqdm(test_df.iterrows(), total=test_df.shape[0]):
+        ont_comp_genes = ont_row[gene_col].split(' ')
+        n = len(ont_comp_genes)
+        for comp, row in ref_df.iterrows():
+            comp_genes = row[gene_col].split(' ')
+            N = len(comp_genes)
             overlap_genes = list(set(ont_comp_genes).intersection(set(comp_genes)))
             x = len(overlap_genes)
-            ref_df.at[ont_comp, comp] = hypergeom.sf(x - 1, M, n, N) # calculare the hypergeometric distribution 
+            hyper_df.at[ont_comp, comp] = hypergeom.sf(x - 1, M, n, N) # calculate the hypergeometric distribution 
             ji_df.at[ont_comp, comp] = jaccard(set(ont_comp_genes), set(comp_genes)) # calculate the jaccard score
-            if ji_df.at[ont_comp, comp] == 0 and ref_df.at[ont_comp, comp] < fdr_thre:
-                print('=== no overlap, but pval < fdr_thre ===')
-                print(ont_comp, comp, ji_df.at[ont_comp, comp], ref_df.at[ont_comp, comp])
-                print(ont_comp_genes)
-                print(comp_genes)
-                print('=== no overlap, but pval < fdr_thre ===')
-                sys.exit()
-    fdr = multipletests(ref_df.values.flatten(), method='fdr_bh')[1]
-    fdr_df = pd.DataFrame(fdr.reshape(ref_df.shape), index=ont_ts.index, columns=ref_file.index, dtype=float)
+    fdr = multipletests(hyper_df.values.flatten(), method='fdr_bh')[1]
+    fdr_df = pd.DataFrame(fdr.reshape(hyper_df.shape), index=test_df.index, columns=ref_df.index, dtype=float)
     
     # 1 if greater than threshold     
     fdr_df[fdr_df > fdr_thre] = 1
@@ -57,61 +51,67 @@ def run_hypergeo_enrichr(ont_ts, hierarchygenes, ref_file, fdr_thre=0.01, ji_thr
     fdr_df[ji_df == 0] = 1
     # filter out those that don't meet ji threshold
     fdr_df[ji_df < ji_thre] = 1
+
     return fdr_df
 
 
 
-parser = argparse.ArgumentParser(description='Analyze each system in the given hierarchy.')
-parser.add_argument('--infname', help='Process number of the test node attributes file')
-parser.add_argument('--refname', help='Process number of the reference node attributes file')
-parser.add_argument('--w_root', action='store_true', help='Do analysis for root term (largest term).')
-parser.add_argument('--minTermSize', default=4, type=int)
-parser.add_argument('--FDRthre', default=1.0, type=float)
-# parser.add_argument('--FDRthre', default=0.001, type=float)
-parser.add_argument('--JIthre', default=0.4, type=float)
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Analyze each system in the given hierarchy.')
+    parser.add_argument('--infname', help='Process number of the test node attributes file')
+    parser.add_argument('--refname', help='Process number of the reference node attributes file')
+    parser.add_argument('--w_root', action='store_true', help='Do analysis for root term (largest term).')
+    parser.add_argument('--minTermSize', default=4, type=int)
+    parser.add_argument('--FDRthre', default=1.0, type=float)
+    # parser.add_argument('--FDRthre', default=0.001, type=float)
+    parser.add_argument('--JIthre', default=0.4, type=float)
+    args = parser.parse_args()
 
-f = f"sc_node_attributes_{args.infname}.csv"
-ref_f = f"sc_node_attributes_{args.refname}.csv"
-minTermSize = args.minTermSize
-fdrthre = args.FDRthre
-jithre = args.JIthre
+    f = f"sc_node_attributes_{args.infname}.csv"
+    ref_f = f"sc_node_attributes_{args.refname}.csv"
+    minTermSize = args.minTermSize
+    fdrthre = args.FDRthre
+    jithre = args.JIthre
 
-def load_node_attributes_file(f):
-    if not os.path.exists(f):
-        print(f)
-        raise ValueError('Input node attributes file does not exist!')
+    def load_node_attributes_file(f):
+        if not os.path.exists(f):
+            print(f)
+            raise ValueError('Input node attributes file does not exist!')
 
-    if os.path.getsize(f) == 0:
-        print('=== No term in hierarchy! ===')
+        if os.path.getsize(f) == 0:
+            print('=== No term in hierarchy! ===')
+            sys.exit()
+
+        df = pd.read_csv(f)
+        df = df[["CD_MemberList_Size", "CD_MemberList", "HiDeF_persistence"]]
+        df.columns = ['tsize', 'genes', 'stability']
+        return df
+
+    df = load_node_attributes_file(f)
+    df_ref = load_node_attributes_file(ref_f)
+
+    root_size = df_ref['tsize'].max()
+    hierarchygenes = set(df_ref[df_ref['tsize'] == df_ref['tsize'].max()]['genes'].str.split(' ').sum())
+
+    if args.w_root:
+        df = df[df['tsize'] >= minTermSize]
+        df_ref = df_ref[df_ref['tsize'] >= minTermSize]
+    else:
+        df_ref = df_ref[(df_ref['tsize'] >= minTermSize) & (df_ref['tsize'] < root_size)]
+        df = df[(df['tsize'] >= minTermSize) & (df['tsize'] < root_size)]
+        
+    if (df_ref.shape[0] == 0) | (df.shape[0] == 0):
+        print('=== No system left after size filter ===')
         sys.exit()
-
-    df = pd.read_csv(f)
-    df = df[["CD_MemberList_Size", "CD_MemberList", "HiDeF_persistence"]]
-    df.columns = ['tsize', 'genes', 'stability']
-    return df
-
-df = load_node_attributes_file(f)
-df_ref = load_node_attributes_file(ref_f)
-
-root_size = df_ref['tsize'].max()
-hierarchygenes = set(df_ref[df_ref['tsize'] == df_ref['tsize'].max()]['genes'].str.split(' ').sum())
-
-if args.w_root:
-    df = df[df['tsize'] >= minTermSize]
-    df_ref = df_ref[df_ref['tsize'] >= minTermSize]
-else:
-    df_ref = df_ref[(df_ref['tsize'] >= minTermSize) & (df_ref['tsize'] < root_size)]
-    df = df[(df['tsize'] >= minTermSize) & (df['tsize'] < root_size)]
-    
-if (df_ref.shape[0] == 0) | (df.shape[0] == 0):
-    print('=== No system left after size filter ===')
-    sys.exit()
-    
-    
-#run the analysis 
-hypergeom_enrich_result = run_hypergeo_enrichr(df.copy(), hierarchygenes, df_ref, fdr_thre=fdrthre, minCompSize=minTermSize,  ji_thre =jithre)
-output_file = f'{args.infname}_{args.refname}_hypergeom.csv'
-print(f'output_file = {output_file}')
-hypergeom_enrich_result.to_csv(output_file)
-print('=== finished analyze_hidef_output ===')
+        
+        
+    #run the analysis 
+    # hypergeom_enrich_result, gene_overlaps = run_hypergeo_enrichr(df.copy(), hierarchygenes, df_ref, fdr_thre=fdrthre, minCompSize=minTermSize,  ji_thre =jithre)
+    hypergeom_enrich_result, gene_overlaps = run_hypergeo_enrichr(df.copy(), df_ref, fdr_thre=fdrthre, minCompSize=minTermSize,  ji_thre =jithre)
+    hyper_output_file = f'{args.infname}_{args.refname}_hypergeom.csv'
+    gene_output_file = f'{args.infname}_{args.refname}_gene_overlaps.pkl'
+    print(f'Test resuls saved in {hyper_output_file}')
+    print(f'Gene overlaps saved in {gene_output_file}')
+    hypergeom_enrich_result.to_csv(hyper_output_file)
+    pkl.dump(gene_overlaps, open(gene_output_file, 'wb'))
+    print('=== finished analyze_hidef_output ===')
